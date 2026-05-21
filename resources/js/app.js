7dab3718d58b88f7b4d94bb260f94b701277b0fb
Akbar Maulana <104122972+akbarmaulanad22@users.noreply.github.com>
2026-05-21 08:16:00 +0700

    feat: disable submit buttons globally on form submit (#149)

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,10 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
+
+document.addEventListener('submit', (event) => {
+    event.target.querySelectorAll('[type=submit]').forEach((btn) => {
+        btn.disabled = true;
+        btn.classList.add('opacity-50', 'cursor-not-allowed');
+    });
+});
